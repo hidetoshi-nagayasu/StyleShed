59f25fa477d37fb8c8d7a660a0ac117b824b2ee5
vagrant <vagrant@localhost.localdomain>
2020-12-29 16:59:03 +0000

    modify pm2 config.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,15 @@
 module.exports = {
   apps : [{
-    script: 'app.js',
-    watch: '.'
-  }, {
-//    script: './service-worker/',
-//    watch: ['./service-worker']
-  }],
-
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
+    name: 'StyleShed',
+    script: 'npm start',
+    watch: '.',
+    env: {
+        PORT: 3000,
+        NODE_ENV: 'development',
+    },
+    env_production: {
+        PORT: 3000,
+        NODE_ENV: 'production',
     }
-  }
+  }]
 };
